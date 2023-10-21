b6334baa77e218bb90c267db4f1925cfcc697be3
wongyichen <wongyichen@163.com>
2023-10-21 13:45:58 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,37 @@
-import {ReactNode, Component, ReactElement, JSXElementConstructor, RefObject, FC} from 'react'
+import {ReactNode, ReactElement, JSXElementConstructor, RefObject } from 'react'
 
 export interface ComponentReactElement {
-  children?: ReactNode | ReactNode[] | ReactElement<any, string | JSXElementConstructor<any>> | null | undefined | any
+    children?: ReactNode | ReactNode[] | ReactElement<any, string | JSXElementConstructor<any>> | null | undefined | any
 }
 
 export type KeepAliveRef = {
-  getCaches: () => Array<{ name: string; ele?: ReactNode }>
-  /**
-   * 清除指定缓存
-   * @param name
-   */
-  removeCache: (name: string) => void
-  /**
-   * 清除所有缓存
-   */
-  cleanAllCache: () => void
-  /**
-   * 清除其他缓存 除了当前的
-   */
-  cleanOtherCache: () => void
+    getCaches: () => Array<{ name: string; ele?: ReactNode }>
+    /**
+     * 清除指定缓存
+     * @param name
+     */
+    removeCache: (name: string) => void
+    /**
+     * 清除所有缓存
+     */
+    cleanAllCache: () => void
+    /**
+     * 清除其他缓存 除了当前的
+     */
+    cleanOtherCache: () => void
 }
 
 interface KeepAliveProps extends ComponentReactElement {
-  activeName: string
-  include?: Array<string>
-  exclude?: Array<string>
-  maxLen?: number
-  cache?: boolean
-  aliveRef?: RefObject<KeepAliveRef>
-  children?: ReactNode | ReactNode[] | ReactElement<any, string | JSXElementConstructor<any>> | null | undefined | any
+    activeName: string
+    include?: Array<string>
+    exclude?: Array<string>
+    maxLen?: number
+    cache?: boolean
+    aliveRef?: RefObject<KeepAliveRef>
+    children?: ReactNode | ReactNode[] | ReactElement<any, string | JSXElementConstructor<any>> | null | undefined | any
 }
 
-export const KeepAlive= (props: KeepAliveProps) => JSX.Element
+const KeepAlive = (props: KeepAliveProps) => JSX.Element
 
 export function useOnActive(cb: () => any, skipMount?: boolean): RefObject<HTMLDivElement>
 
